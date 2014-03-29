7ea0724c6a509dd894d555ff4000bb15cfd84905
Toby Champion <toby@tobychampion.com>
2014-03-29 16:03:09 -0700

    Handle user meta data better, including serialized values
    - meta values are always represented as arrays
    - serialized values are represented as dictionaries with 'unserialized'
      and 'serialized' keys. only 'unserialized' is used when you update.
    - if you send it a single value, it makes it into an array (to be kind)
    - doesn't deal with multiple values coming back yet (see comments)

--- lib/class-wp-json-users.php
+++ lib/class-wp-json-users.php
@@ -107,8 +107,15 @@ class WP_JSON_Users {
 		return $response;
 	}
 
+	/**
+	 * @param WP_User $user
+	 * @param string $context
+	 * @return array
+	 */
 	protected function prepare_user( $user, $context = 'view' ) {
 		// We're ignoring $fields for now, so you get all these fields
+		// http://codex.wordpress.org/Function_Reference/get_metadata
+		// http://code.tutsplus.com/articles/mastering-wordpress-meta-data-understanding-and-using-arrays--wp-34596
 		$user_fields = array(
 			'ID' => $user->ID,
 			'login' => $user->user_login,
@@ -118,12 +125,53 @@ class WP_JSON_Users {
 			'url' => $user->user_url,
 			'registered' => $user->user_registered,
 			'display_name' => $user->display_name,
-			'first_name' => $user->first_name,
-			'last_name' => $user->last_name,
-			'nickname' => $user->nickname,
-			'description' => $user->description,
+			'first_name' => $user->first_name,  // is also in meta
+			'last_name' => $user->last_name,  // is also in meta
+			'nickname' => $user->nickname,  // is also in meta
+			'description' => $user->description,  // is also in meta
 		);
+		// https://codex.wordpress.org/Metadata_API
+		// "Objects may contain multiple metadata entries that share the same key and differ only in their value."
+		// JSON can have duplicate keys in an object: http://stackoverflow.com/a/19927061/76452
+		// "The names within an object SHOULD be unique"... but clearly this is a bad idea.
+		// So we can't just use a dictionary here...
+		// Ah, I've read that "When you store multiple key/value pairs for a post, that key is turned into
+		// an array." So I guess we're okay. I'll ignore this for now.
+		// Wooah there's serialization too. My assumption is that serialized data is gonna be useless to users.
+		// I'm using Python, for one thing. So we'll maybe_unserialize each metadata value.
+		// Okay, so each metadata thing comes out as an array. I get it. Because there can be multiple values per key.
+		// So we need to maybe_unserialize each member. But how to know which to serialize when we write back?
+		// Maybe we need a separate entity for a serialized PHP structure. It can be a dictionary, with a special
+		// key, 'unserialized'. We could then have a 'serialized' key too. When updating/creating, 'serialized' would
+		// take precedence, unless there was an error.
+		// TODO: consider representing single arrays just as the item (we can already cope with this coming back)
+		// TODO: check-in with WP-API folks about true/false (in metadata)... I imagine we should just pass the
+		// strings back, and for common fields just send nicer stuff back outside of the meta
+		$user_meta = get_user_meta( $user->ID );
+
+		// TODO: Don't use "meta" as it's used in the JSON response for eg links.self; use user_meta instead
+		// TODO: Add our own "meta" stuff with links.self
+		$user_fields['meta'] = $this->prepare_meta( $user_meta );
 		return $user_fields;
+	}
+
+	protected function prepare_meta( $meta ) {
+		$prepared_meta = [];
+		foreach ( $meta as $meta_key => $meta_values ) {
+			$prepared_meta[$meta_key] = [];
+			foreach ( $meta_values as $meta_value ) {
+				if ( is_serialized( $meta_value ) ) {
+					$prepared_meta_value = [
+						'unserialized' => @unserialize( $meta_value ),
+						'serialized' => $meta_value,  // for completeness, testing, debugging
+					];
+			    } else {
+					$prepared_meta_value = $meta_value;
+				}
+				$prepared_meta[$meta_key][] = $prepared_meta_value;
+			}
+		}
+		return $prepared_meta;
 	}
 
 	/**
@@ -213,7 +261,8 @@ class WP_JSON_Users {
 	// user is a WP_User; $data is an array of fields to update
 	protected function update_user( $user, $data ) {
 
-		// Won't let them update these fields: ID, login, pass, registered,
+		// Won't let them update these fields: ID, login, pass, registered (silently ignored)
+		// TODO: Raise an exception if they try to update those. Ignore ID though.
 		// WP won't let you change login (username)
 		// Note that you can pass wp_update_user() an array of fields to
 		// update; they're not the same as being used here (and probably
@@ -251,6 +300,48 @@ class WP_JSON_Users {
 			$user->description = $data[ 'description' ];
 		}
 
+		if ( ! empty( $data['meta'] ) ) {
+			// https://codex.wordpress.org/Function_Reference/update_metadata
+			// https://codex.wordpress.org/Function_Reference/update_user_meta
+			// "update_user_meta does not delete the meta if the new value is empty"
+			// TODO: Do something about that. Maybe treat each metadata item as a separate resource?
+			foreach ( $data['meta'] as $meta_key => $meta_value_or_values ) {
+				// "The new desired value of the meta_key, which must be different from the existing value.
+				// Arrays and objects will be automatically serialized"
+				// Really? It MUST be different? Actually, it just returns false. And we don't care.
+				// TODO: Something's wrong here. Eg we get wp_user_level back as u'wp_user_level': [u'0']
+				// but if we send that, it gets serialized as "[u'a:1:{i:0;s:1:"0";}'],"...
+				// need to test round-trip metadata properly... we need to unserialize stuff... is WP-API doing this?
+				// OMG this is a whole issue: http://wpgarage.com/tips/data-portability-and-data-serialization-in-wordpress/
+				// s2member does this, for one thing.
+				// I guess we might need to use some heuristic: if the existing value is serialized, serialize before updating
+				// https://codex.wordpress.org/Function_Reference/maybe_unserialize
+				// TODO: Error out here if $meta_values is not an array (and pass that back up to the server)
+				// I don't get it. You get an array from get_user_meta, but if you provide one, it serializes it. Uh?
+
+				// This is so users can provide single values rather than an array
+				if ( ! is_array($meta_value_or_values)) {
+					$meta_values = array( $meta_value_or_values );
+				} else {
+					$meta_values = $meta_value_or_values;
+				}
+
+				foreach ( $meta_values as $meta_value) {
+					// We're letting update_user_meta deal with multiple values... I can't see how to get it to
+					// accept multiple values (from an array) without ending up serializing it.
+					// TODO: Fix that
+					if ( is_array( $meta_value ) ) {
+						$unprepared_meta_value = $meta_value['unserialized']; // ignore any 'serialized'
+						update_user_meta( $user->ID, $meta_key, $unprepared_meta_value );
+					} else {
+						$unprepared_meta_value = $meta_value;
+						update_user_meta( $user->ID, $meta_key, $unprepared_meta_value );
+					}
+				}
+				// update_user_meta( $user->ID, $meta_key, $unprepared_meta_values );
+			}
+
+		}
 	}
 
 }
